7507327995f596b50d9f83826239586b19d3156f
Isabela <159652261+hiimizzy@users.noreply.github.com>
2026-02-26 12:01:39 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// ATENÇÃO: Este arquivo contém chaves de API e não deve ser enviado para o GitHub.
+// Ele já está incluído no .gitignore para sua segurança.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlVcU8qGHHobZ1_SMaB3uZ9QwfsEEVq2M",
+  authDomain: "lvgr-2119a.firebaseapp.com",
+  projectId: "lvgr-2119a",
+  storageBucket: "lvgr-2119a.firebasestorage.app",
+  messagingSenderId: "570796412382",
+  appId: "1:570796412382:web:944b16471cf20e40a2a14f"
+};
